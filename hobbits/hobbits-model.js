f74cf07f06ffb7ba480b97f2f594a10577fb2468
Jason Maurer <jason@maur.co>
2020-07-15 14:00:06 -0600

    Some cleanup

--- hobbits/hobbits-model.js
+++ hobbits/hobbits-model.js
@@ -1,29 +1,31 @@
 const db = require("../data/config")
 
-async function create(data) {
-	return null
-}
-
-async function update(id, data) {
-	return null
-}
-
-function remove(id) {
-	return null
-}
-
 function find() {
 	return db("hobbits")
 }
 
 function findById(id) {
-	return null
+	return db("hobbits").where({ id }).first()
+}
+
+async function create(data) {
+	const [id] = await db("hobbits").insert(data)
+	return findById(id)
+}
+
+async function update(id, data) {
+	await db("hobbits").where({ id }).update(data)
+	return findById(id)
+}
+
+function remove(id) {
+	return db("hobbits").where({ id }).del()
 }
 
 module.exports = {
+	find,
+	findById,
 	create,
 	update,
 	remove,
-	find,
-	findById,
 }
